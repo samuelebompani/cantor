4d50ff2deadce5de8f1a7a3fe73d8fa84e079800
samuele <samuelebompani@gmail.com>
2021-03-12 16:30:59 +0100

    first commit

---
+++ cantor.py
@@ -0,0 +1,8 @@
+def cantor(x,y):
+    return (x+y)*(x+y+1)/2+y+1
+
+def sx(n):
+    return n
+
+def dx(n):
+    return n
---
+++ test.py
@@ -0,0 +1,8 @@
+import cantor
+
+assert(cantor.cantor(0,0)==1)
+assert(cantor.cantor(0,3)==10)
+assert(cantor.cantor(4,0)==11)
+assert(cantor.cantor(3,2)==18)
+
+print("Ok")
