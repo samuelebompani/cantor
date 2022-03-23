ef02cc10256f84e5b565e8fdb583d6075e9477b1
samuele <samuelebompani@gmail.com>
2022-03-23 17:51:13 +0100

    random program

--- programEncoder.py
+++ programEncoder.py
@@ -1,21 +1,23 @@
 import cantor
 import integerList
+import random
 
 def decode_instruction(i):
     if(i%3 == 0):
         tmp = int(i/3)
-        print("R_"+str(tmp)," = R_"+str(tmp)," + 1\n")
+        return "R_"+str(tmp)+" = R_"+str(tmp)+" + 1"
     elif(i%3 == 1):
         tmp = int(i/3)
-        print("R_"+str(tmp)," = R_"+str(tmp)," - 1\n")
+        return "R_"+str(tmp)+" = R_"+str(tmp)+" - 1"
     else:
         s = cantor.sx(int((i+1)/3))
         d = cantor.dx(int((i+1)/3))
-        print("If R_"+str(s),"== 0 then goto",d)
+        return "If R_"+str(s)+"== 0 then goto"+str(d)
     
 def decode(P):
     instructions = integerList.decode(P)
     print(instructions)
     return list(map(lambda x: decode_instruction(x),instructions))
 
-decode(1756262107)
+def random_program():
+    return decode(random.randint(0, 1999999191919191919))
